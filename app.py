89fdd72029417f425dda36b02e5cfb670f2e8abc
“hyunji0618” <hyunjik000618@gmail.com>
2024-11-11 21:27:39 -0600

    add deployment path

--- app.py
+++ app.py
@@ -63,4 +63,6 @@ def second_page():
     return render_template('second_page.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+
+    
