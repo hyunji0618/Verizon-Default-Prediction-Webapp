3f8674dd2ac4a18199a93532cab9a3de76487d6b
“hyunji0618” <hyunjik000618@gmail.com>
2024-11-13 13:19:54 -0600

    Update with correct app route

--- app.py
+++ app.py
@@ -59,9 +59,9 @@ def second_page():
         return render_template('result.html', y_pred_class=y_pred_class)
     return render_template('second_page.html')
 
-#if __name__ == '__main__':
-#    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+
+#if __name__ == '__main__':
+#    app.run(debug=True)
 
